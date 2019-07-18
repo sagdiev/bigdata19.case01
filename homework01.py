3529e72322c2341c326ff71a66ede64d49455668
Artem Sagdiev <sagdiev@gmail.com>
2019-07-18 21:23:31 +0300

    After Lesson 02

---
+++ homework01.py
@@ -0,0 +1,14 @@
+from yahoo import read_symbols, YAHOO_HTMLS
+
+def scrape_descriptions_sync():
+    "DZ Scrape companies descriptions. sync"
+    # TODO: Second DZ с помощь urllib
+    # прочитать Symbols, for symbol in tqdm(symbols)
+    # исользовать urllib get запросы на yahoo и полученное записывать в файл с помощью
+    # добавить tqdm(symbols)
+
+def main():
+    scrape_descriptions_async()
+
+if __name__ == '__main__':
+    main()
